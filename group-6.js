07a8d04da1b90a2214aead0b07ef3e4199c8de9b
onleibrahim <ibrahimsalat019@gmail.com>
2024-05-05 12:53:31 +0300

    group-6 switch-statement presentation

---
+++ group-6.js
@@ -0,0 +1,53 @@
+// switch statement in javascript presented by group-6
+
+
+let person = "abdi"
+let registrationNumber; 
+
+switch (person){
+  case "abubakar":
+  registrationNumber = "s110/p2244/23"
+  break;
+  case "ibrahim":
+  registrationNumber = "s110/p1335/23"
+  break;
+  case "noor":
+  registrationNumber = "s110/p2277/23"
+  break;
+  case "abdi":
+  registrationNumber = "s110/p2288/23"
+  break;
+
+  default:
+    registrationNumber = "not found"
+
+}
+console.log(registrationNumber);
+
+
+
+
+    
+    
+    
+// let userName = 4;
+// let studentName;
+
+// switch(userName){
+//     case 1:
+//         studentName = "abubakar";
+//         break;
+//     case 2:
+//         studentName = "ibrahim";
+//         break;
+//     case 3:
+//         studentName = "noor";
+//         break;
+//     case 4:
+//         studentName = "hassan";
+//         break;
+//     default:
+//         console.log("not found");
+// }
+
+// console.log(studentName);
